65798614539b4a5aa7db4a75d98085b7915d6d96
Zorica Mitrovic <zorica_mitrovic@mail.com>
2021-06-01 23:50:16 +0200

    Scroll Down Finished

--- js/index.js
+++ js/index.js
@@ -4,3 +4,12 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+function scroldown(){
+  window.scroll({
+    top: 25000, 
+    left: 0, 
+    behavior: 'smooth' 
+  });
+
+}
